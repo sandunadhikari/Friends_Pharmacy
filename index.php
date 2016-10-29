559fcf64ecb3bbd7259f9a3fa502ac2cf6d6105b
Mithsen <MithsenDeSilva@gmai.com>
2016-10-29 12:58:58 +0530

    edit index

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <html>
 <body>
-<a href="main/main.php">click here to go to the main page of the pharmacist</a>
-<a href="web/index.php">click here to go to the web site of </a>
+    
+    <a href="main/main.php">click here to go to the main page of the pharmacist</a><br>
+    
+    <a href="web/index.php">click here to go to the web site of the customer</a>
 </body>
-</html>>
+</html>
 
 
 
