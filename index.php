c319c32d1c36fffc6436eace1b1a6e5a277535a8
Mithsen <MithsenDeSilva@gmai.com>
2016-10-29 12:49:26 +0530

    edit index

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<?php
-   
-include 'main/main/main.php';
-?>
-
-
-
-
+<html>
+<body>
+<a href="main/main.php">click here to go to the main page of the pharmacist</a>
+<a href="web/index.php">click here to go to the web site of </a>
+</body>
+</html>>
 
 
 
