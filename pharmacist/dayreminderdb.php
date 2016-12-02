43455214ac5b22caebdacebc791b0c799d23540d
Mithsen <MithsenDeSilva@gmai.com>
2016-12-02 15:40:37 +0530

    fix error

--- pharmacist/dayreminderdb.php
+++ pharmacist/dayreminderdb.php
@@ -8,7 +8,7 @@ switch ($_GET['type']) {
     case 'get' : {
       $search = $_GET['search'];
 
-$sql = "SELECT reminderday.nic,reminderday.contactno,reminderday.medname,reminderday.instruction,reminder.quantity,reminder.time1,reminder.time2,reminder.time3,reminder.startdate,reminder.enddate FROM reminderday INNER JOIN customer reminderday.nic= customer.nic AND customer.first_name LIKE '%$search%'"
+$sql = "SELECT reminderday.nic,reminderday.contactno,reminderday.medname,reminderday.instruction,reminder.quantity,reminder.time1,reminder.time2,reminder.time3,reminder.startdate,reminder.enddate FROM reminderday INNER JOIN customer reminderday.nic= customer.nic AND customer.first_name LIKE '%$search%'";
         $result = mysqli_query($con, $sql);
         $res_data = array();
         while($row = mysqli_fetch_assoc($result)) {
